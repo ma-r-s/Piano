b54d23ee0ab9e9ff401844973ff66fdcec75c9dc
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2022-08-31 15:06:47 -0500

    Modules

---
+++ src/routes/Keyboard.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  let nota = "";
+  let notasBlancas: string[] = ["C", "D", "E", "F", "G", "A", "B"];
+</script>
+
+<div class="flex justify-center my-8">
+  <div class="absolute flex space-x-1">
+    {#each notasBlancas as notaB}
+      <button
+        on:click={() => {
+          nota = notaB;
+        }}
+        class="drop-shadow-lg h-28 w-8 bg-white rounded-md hover:bg-gray-200"
+      />
+    {/each}
+  </div>
+  <div class="absolute flex space-x-3">
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <div class="h-16 w-6" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <div class="h-16 w-6" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <div class="h-16 w-6" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+    <button class="h-16 w-6 bg-black rounded-b-full hover:bg-gray-600" />
+  </div>
+</div>
