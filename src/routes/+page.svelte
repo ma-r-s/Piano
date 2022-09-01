fc3ea2f0a4a73bf48dd636218022962eaa764e29
Mario Ruiz <m.ruizs@uniandes.edu.co>
2022-09-01 15:11:53 -0500

    Fixed identation (tabs instead of spaces and new line for multiline html elements)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import Keyboard from "./Keyboard.svelte";
-    let nota: string = "";
-    const handleMessage = (event: any) => {
-        nota = event.detail;
-    };
+	import Keyboard from "./Keyboard.svelte";
+	let nota: string = "";
+	const handleMessage = (event: any) => {
+		nota = event.detail;
+	};
 </script>
 
 <div class="flex min-h-screen flex-col items-center bg-slate-100">
-    <h1 class=" p-6 text-3xl font-bold underline">Nota: {nota}</h1>
-    <Keyboard octaves={2} on:notePressed={handleMessage} />
+	<h1 class=" p-6 text-3xl font-bold underline">Nota: {nota}</h1>
+	<Keyboard octaves={2} on:notePressed={handleMessage} />
 </div>
--- src/routes/Keyboard.svelte
+++ src/routes/Keyboard.svelte
@@ -1,36 +1,39 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
-    const dispatch = createEventDispatcher();
-    export let octaves = 2;
-    const natural: boolean[] = [
-        true,
-        false,
-        true,
-        false,
-        true,
-        true,
-        false,
-        true,
-        false,
-        true,
-        false,
-        true,
-    ];
+	import { createEventDispatcher } from "svelte";
+	const dispatch = createEventDispatcher();
+	export let octaves = 2;
+	const natural: boolean[] = [
+		true,
+		false,
+		true,
+		false,
+		true,
+		true,
+		false,
+		true,
+		false,
+		true,
+		false,
+		true,
+	];
 </script>
 
 <div class="flex">
-    {#each Array(octaves * 12 + 1) as _, note}
-        {#if natural[note % 12]}
-            <button
-                on:click={() => {
-                    dispatch("notePressed", note);
-                }}
-                class="mx-0.5 h-28 w-8 rounded-md bg-white drop-shadow-lg hover:bg-gray-200" />
-        {:else}<button
-                on:click={() => {
-                    dispatch("notePressed", note);
-                }}
-                class="z-10 -mx-3 h-16 w-6 rounded-b-full bg-black hover:bg-gray-600" />
-        {/if}
-    {/each}
+	{#each Array(octaves * 12 + 1) as _, note}
+		{#if natural[note % 12]}
+			<button
+				on:click={() => {
+					dispatch("notePressed", note);
+				}}
+				class="mx-0.5 h-28 w-8 rounded-md bg-white drop-shadow-lg hover:bg-gray-200"
+			/>
+		{:else}
+			<button
+				on:click={() => {
+					dispatch("notePressed", note);
+				}}
+				class="z-10 -mx-3 h-16 w-6 rounded-b-full bg-black hover:bg-gray-600"
+			/>
+		{/if}
+	{/each}
 </div>
